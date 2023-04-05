73202196ff1b456d4f77c88a46121cc9b1256461
than <than@thans-air.lan>
2023-04-05 22:47:44 +0300

    update to 20.04, customize ports, correct passv_address

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM ubuntu:22.04
 
 RUN apt-get update && \
 		apt-get install -y --no-install-recommends vsftpd db-util && \
@@ -8,9 +8,9 @@ ENV FTP_USER admin
 ENV FTP_PASS admin
 ENV PASV_ADDRESS REQUIRED
 
-COPY ftp/vsftpd.conf /etc/vsftpd/
-COPY ftp/vsftpd_virtual /etc/pam.d/
-COPY ftp/run-vsftpd.sh /usr/sbin/
+COPY vsftpd.conf /etc/vsftpd/
+COPY vsftpd_virtual /etc/pam.d/
+COPY run-vsftpd.sh /usr/sbin/
 
 RUN chmod +x /usr/sbin/run-vsftpd.sh && \
 		mkdir -p /var/run/vsftpd/empty
--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -11,7 +11,14 @@ if [ "$PASV_ADDRESS" = "REQUIRED" ]; then
 	echo "Please insert IPv4 address of your host"
 	exit 1
 fi
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+if ! grep pasv_address /etc/vsftpd/vsftpd.conf > /dev/null; then
+	echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+else
+	sed -i "s/^pasv_address=/pasv_address=${PASV_ADDRESS}/g" /etc/vsftpd/vsftpd.conf
+fi
+
+sed -i "s/_MIN_PORT_/${PASV_MIN_PORT}/g" /etc/vsftpd/vsftpd.conf
+sed -i "s/_MAX_PORT_/${PASV_MAX_PORT}/g" /etc/vsftpd/vsftpd.conf
 
 # Run vsftpd:
 vsftpd /etc/vsftpd/vsftpd.conf
